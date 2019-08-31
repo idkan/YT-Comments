deac7eb9ba06160a00cb58d801602e37f4f8d67b
Abraham <abraham.serena.lk@gmail.com>
2019-08-30 23:46:21 -0500

    Comment feature solved

--- YTAuto-Commets.py
+++ YTAuto-Commets.py
@@ -1,13 +1,11 @@
-import os
 import sys
+from random import sample
 
-import httplib2
+import googleapiclient
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-
-from oauth2client.client import flow_from_clientsecrets
+import google_auth_oauthlib.flow
+from apiclient.errors import HttpError
 from oauth2client.file import Storage
-from oauth2client.tools import argparser, run_flow
 
 # The CLIENT_SECRETS_FILE variable specifies the name of a file that contains
 # the OAuth 2.0 information for this application, including its client_id and
@@ -21,48 +19,26 @@ from oauth2client.tools import argparser, run_flow
 #   https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
 CLIENT_SECRETS_FILE = "client_secret.json"
 
-# This variable defines a message to display if the CLIENT_SECRETS_FILE is
-# missing.
-MISSING_CLIENT_SECRETS_MESSAGE = """
-WARNING: Please configure OAuth 2.0
-
-To make this sample run you will need to populate the client_secrets.json file
-found at:
-
-   %s
-
-with information from the Developers Console
-https://console.developers.google.com/
-
-For more information about the client_secrets.json file format, please visit:
-https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
-""" % os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                   CLIENT_SECRETS_FILE))
-
 # This OAuth 2.0 access scope allows for full read/write access to the
 # authenticated user's account.
-YOUTUBE_READ_WRITE_SCOPE = "https://www.googleapis.com/auth/youtube.force-ssl"
+scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
 # Set DEVELOPER_KEY to the "API key" value from the Google Developers Console:
 # https://console.developers.google.com/project/_/apiui/credential
 # Please ensure that you have enabled the YouTube Data API for your project.
-DEVELOPER_KEY = "AIzaSyCB3AQSOr4wOpEN50uGNUl01VjUx4CqOmg"
+DEVELOPER_KEY = "REPLACE ME"
 
 
 def get_authenticated_service():
-    flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE,
-                                   scope=YOUTUBE_READ_WRITE_SCOPE,
-                                   message=MISSING_CLIENT_SECRETS_MESSAGE)
-    storage = Storage("%s-oauth2.json" % sys.argv[0])
-    credentials = storage.get()
+    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+        CLIENT_SECRETS_FILE, scopes)
 
-    if credentials is None or credentials.invalid:
-        credentials = run_flow(flow, storage)
+    credentials = flow.run_console()
 
-    return build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
-                 http=credentials.authorize(httplib2.Http()))
+    return googleapiclient.discovery.build(
+        YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, credentials=credentials)
 
 
 def get_video_id(youtube_request):
@@ -92,33 +68,38 @@ def get_video_id(youtube_request):
 
 
 def like_video(youtube_request, id_video):
-
     for item in id_video:
         youtube_request.videos().rate(
             id=item,
             rating="like"
         ).execute()
+        print(f'Liked video: {item}')
 
 
-def comment_video(youtube_request, channel_id, id_video, text):
-    insert_result = youtube_request.commentThreads().insert(
-        part="snippet",
-        body=dict(
-            snippet=dict(
-                channelId=channel_id,
-                videoId=id_video,
-                topLevelComment=dict(
-                    snippet=dict(
-                        textOriginal=text)
+def comment_video(youtube_request, channel_id, id_video):
+    comments = ['Buen video #JucaS3laSabe', 'Naves y Nenas por siempre #JucaS3laSabe',
+                'Que naves!! #JucaS3laSabe', '#JucaS3laSabe Siempre presente en tus videos!!',
+                '#JucaS3laSabe Presente!! como siempre', 'Siempre rifado con tus videos Juca! #JucaS3laSabe',
+                '#JucaS3laSabe']
+    for i in id_video:
+        insert_result = youtube_request.commentThreads().insert(
+            part="snippet",
+            body=dict(
+                snippet=dict(
+                    channelId=channel_id,
+                    videoId=i,
+                    topLevelComment=dict(
+                        snippet=dict(
+                            textOriginal=sample(comments, k=1))
+                    )
                 )
             )
-        )
-    ).execute()
+        ).execute()
 
-    comment = insert_result["snippet"]["topLevelComment"]
-    author = comment["snippet"]["authorDisplayName"]
-    text = comment["snippet"]["textDisplay"]
-    print("Inserted comment for %s: %s" % (author, text))
+        comment = insert_result["snippet"]["topLevelComment"]
+        author = comment["snippet"]["authorDisplayName"]
+        text = comment["snippet"]["textDisplay"]
+        print(f'Inserted comment in Video id: {i} for: {author}: {text}')
 
 
 if __name__ == "__main__":
@@ -139,9 +120,9 @@ if __name__ == "__main__":
 
         try:
             like_video(authenticated_service, youtube_videoId_links)
-            comment_video(authenticated_service, 'UCqnFo90nZIcOxbfMekqnKiQ', 'Hh_8hp-c65c', '#JucaS3laSabe')
+            comment_video(authenticated_service, 'UCqnFo90nZIcOxbfMekqnKiQ',
+                          youtube_videoId_links)
         except HttpError as e:
             print(f'An HTTP error {e.resp.status} occurred: {e.content}')
         else:
             print('Video has been liked.')
-
